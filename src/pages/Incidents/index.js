d74d1f561dd44fd1f1d4bbe58015e6ec7f1612b3
Gabriel Ferreira <gabreeiel@gmail.com>
2020-03-26 20:15:46 -0300

    Creating Incidents page.

--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,8 +1,61 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image, Text, TouchableOpacity, FlatList } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
+import logoImage from "../../assets/logo.png";
+import styles from "./styles";
 
 function Incidents() {
-  return <View />;
+  const navigation = useNavigation();
+
+  function navigateToDetail() {
+    navigation.navigate("Detail");
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImage} />
+        <Text style={styles.headerText}>
+          Total: <Text style={styles.headerTextBold}>0 Incidents</Text>
+        </Text>
+      </View>
+
+      <Text style={styles.title}>Welcome!</Text>
+      <Text style={styles.description}>
+        Choose one of the cases below and save the day.
+      </Text>
+
+      <FlatList
+        style={styles.incidentList}
+        data={[1, 2, 3]}
+        keyExtractor={incident => String(incident)}
+        showsVerticalScrollIndicator={false}
+        renderItem={() => (
+          <View style={styles.incident}>
+            <Text style={styles.incidentProperty}>NGO:</Text>
+            <Text style={styles.incidentValue}>APAE</Text>
+
+            <Text style={styles.incidentProperty}>Incident:</Text>
+            <Text style={styles.incidentValue}>
+              Buy a chair wheelchair to a pacient.
+            </Text>
+
+            <Text style={styles.incidentProperty}>Value:</Text>
+            <Text style={styles.incidentValue}>U$ 120.00</Text>
+
+            <TouchableOpacity
+              style={styles.detailsButton}
+              onPress={navigateToDetail}
+            >
+              <Text style={styles.detailsButtonText}>Show datails</Text>
+              <Feather name="arrow-right" size={16} color="#E02041" />
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  );
 }
 
 export default Incidents;

4eb51451866111a85042fd5003048fc10eb39a62
Gabriel Ferreira <gabreeiel@gmail.com>
2020-03-26 23:24:01 -0300

    Loading incidents

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import "intl";
+import "intl/locale-data/jsonp/en-US";
 import React from "react";
 import Routes from "./src/routes";
 
--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -1,23 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, Image, Text, TouchableOpacity, FlatList } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
+import api from "../../services/api";
 import logoImage from "../../assets/logo.png";
 import styles from "./styles";
 
 function Incidents() {
+  const [incidents, setIncidents] = useState([]);
+  const [total, setTotal] = useState(0);
   const navigation = useNavigation();
 
   function navigateToDetail() {
     navigation.navigate("Detail");
   }
+  async function loadIncidents() {
+    const response = await api.get("incident");
+    setIncidents(response.data);
+    setTotal(response.headers["x-total-count"]);
+  }
 
+  useEffect(() => {
+    loadIncidents();
+  }, []);
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Image source={logoImage} />
         <Text style={styles.headerText}>
-          Total: <Text style={styles.headerTextBold}>0 Incidents</Text>
+          Total: <Text style={styles.headerTextBold}>{total} Incidents</Text>
         </Text>
       </View>
 
@@ -28,21 +39,24 @@ function Incidents() {
 
       <FlatList
         style={styles.incidentList}
-        data={[1, 2, 3]}
-        keyExtractor={incident => String(incident)}
+        data={incidents}
+        keyExtractor={incident => String(incident.id)}
         showsVerticalScrollIndicator={false}
-        renderItem={() => (
+        renderItem={({ item: incident }) => (
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>NGO:</Text>
-            <Text style={styles.incidentValue}>APAE</Text>
+            <Text style={styles.incidentValue}>{incident.name}</Text>
 
             <Text style={styles.incidentProperty}>Incident:</Text>
-            <Text style={styles.incidentValue}>
-              Buy a chair wheelchair to a pacient.
-            </Text>
+            <Text style={styles.incidentValue}>{incident.description}</Text>
 
             <Text style={styles.incidentProperty}>Value:</Text>
-            <Text style={styles.incidentValue}>U$ 120.00</Text>
+            <Text style={styles.incidentValue}>
+              {Intl.NumberFormat("en-US", {
+                style: "currency",
+                currency: "USD"
+              }).format(incident.value)}
+            </Text>
 
             <TouchableOpacity
               style={styles.detailsButton}

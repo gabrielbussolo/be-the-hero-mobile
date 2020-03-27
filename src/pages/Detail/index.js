0b26e1e87def5afe8defa598fb81269f0853a658
Gabriel Ferreira <gabreeiel@gmail.com>
2020-03-26 22:02:12 -0300

    Implementing contact actions, whatsapp and email

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,15 +1,30 @@
 import React from "react";
-import { View, Image, Text, TouchableOpacity } from "react-native";
+import { View, Image, Text, TouchableOpacity, Linking } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
+import * as MailComposer from "expo-mail-composer";
 import styles from "./styles";
 import logoImage from "../../assets/logo.png";
 
 function Detail() {
   const navigation = useNavigation();
-
+  const message =
+    'Hello Company, I am contacting you because I would like to help with the incident"Incident here" with a donation of U$ 120';
+  const phoneNumber = "fakenumber";
   function navigateBack() {
     navigation.goBack();
+  }
+
+  function sendMail() {
+    MailComposer.composeAsync({
+      subject: "Hero of the incident: indedent x",
+      recipients: ["mail@mock.com", "mail@mock.com"],
+      body: message
+    });
+  }
+
+  function sendWhatsApp() {
+    Linking.openURL(`whatsapp://send?phone=${phoneNumber}&text=${message}`);
   }
 
   return (
@@ -33,14 +48,14 @@ function Detail() {
         <Text style={styles.incidentValue}>U$ 120.00</Text>
       </View>
       <View style={styles.contactBox}>
-        <Text style={styles.heroTitle}></Text>
-        <Text style={styles.heroTitle}></Text>
-        <Text style={styles.description}></Text>
+        <Text style={styles.heroTitle}>Save the day!</Text>
+        <Text style={styles.heroTitle}>Be the hero of this incident.</Text>
+        <Text style={styles.description}>Contact:</Text>
         <View style={styles.actions}>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendWhatsApp}>
             <Text style={styles.actionText}>WhatsApp</Text>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.action} onPress={() => {}}>
+          <TouchableOpacity style={styles.action} onPress={sendMail}>
             <Text style={styles.actionText}>E-mail</Text>
           </TouchableOpacity>
         </View>

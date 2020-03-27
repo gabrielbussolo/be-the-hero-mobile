3a302d0a07edce6c19688253153b952decd95d59
Gabriel Ferreira <gabreeiel@gmail.com>
2020-03-26 21:44:45 -0300

    Creating details page.

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,8 +1,52 @@
 import React from "react";
-import { View } from "react-native";
+import { View, Image, Text, TouchableOpacity } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Feather } from "@expo/vector-icons";
+import styles from "./styles";
+import logoImage from "../../assets/logo.png";
 
 function Detail() {
-  return <View />;
+  const navigation = useNavigation();
+
+  function navigateBack() {
+    navigation.goBack();
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Image source={logoImage} />
+        <TouchableOpacity onPress={navigateBack}>
+          <Feather name="arrow-left" size={28} color="#e82041" />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.incident}>
+        <Text style={[styles.incidentProperty, { marginTop: 0 }]}>NGO:</Text>
+        <Text style={styles.incidentValue}>APAE</Text>
+
+        <Text style={styles.incidentProperty}>Incident:</Text>
+        <Text style={styles.incidentValue}>
+          Buy a chair wheelchair to a pacient.
+        </Text>
+
+        <Text style={styles.incidentProperty}>Value:</Text>
+        <Text style={styles.incidentValue}>U$ 120.00</Text>
+      </View>
+      <View style={styles.contactBox}>
+        <Text style={styles.heroTitle}></Text>
+        <Text style={styles.heroTitle}></Text>
+        <Text style={styles.description}></Text>
+        <View style={styles.actions}>
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionText}>WhatsApp</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.action} onPress={() => {}}>
+            <Text style={styles.actionText}>E-mail</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </View>
+  );
 }
 
 export default Detail;

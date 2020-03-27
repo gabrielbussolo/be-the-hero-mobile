c984ac0d0c7425bb6827c9fd48e6eb2c0855f96b
Gabriel Ferreira <gabreeiel@gmail.com>
2020-03-26 23:34:02 -0300

    Ajusting details and contact functions

--- src/pages/Detail/index.js
+++ src/pages/Detail/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { View, Image, Text, TouchableOpacity, Linking } from "react-native";
-import { useNavigation } from "@react-navigation/native";
+import { useNavigation, useRoute } from "@react-navigation/native";
 import { Feather } from "@expo/vector-icons";
 import * as MailComposer from "expo-mail-composer";
 import styles from "./styles";
@@ -8,23 +8,33 @@ import logoImage from "../../assets/logo.png";
 
 function Detail() {
   const navigation = useNavigation();
-  const message =
-    'Hello Company, I am contacting you because I would like to help with the incident"Incident here" with a donation of U$ 120';
-  const phoneNumber = "fakenumber";
+  const route = useRoute();
+  const incident = route.params.incident;
+  const message = `Hello ${
+    incident.name
+  }, I am contacting you because I would like to help with the incident "${
+    incident.title
+  }" with a donation of ${Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD"
+  }).format(incident.value)}`;
+
   function navigateBack() {
     navigation.goBack();
   }
 
   function sendMail() {
     MailComposer.composeAsync({
-      subject: "Hero of the incident: indedent x",
-      recipients: ["mail@mock.com", "mail@mock.com"],
+      subject: `Hero of the incident: ${incident.title}`,
+      recipients: [incident.email],
       body: message
     });
   }
 
   function sendWhatsApp() {
-    Linking.openURL(`whatsapp://send?phone=${phoneNumber}&text=${message}`);
+    Linking.openURL(
+      `whatsapp://send?phone=${incident.whatsapp}&text=${message}`
+    );
   }
 
   return (
@@ -37,15 +47,20 @@ function Detail() {
       </View>
       <View style={styles.incident}>
         <Text style={[styles.incidentProperty, { marginTop: 0 }]}>NGO:</Text>
-        <Text style={styles.incidentValue}>APAE</Text>
-
-        <Text style={styles.incidentProperty}>Incident:</Text>
         <Text style={styles.incidentValue}>
-          Buy a chair wheelchair to a pacient.
+          {incident.name} from {incident.city}/{incident.state}
         </Text>
 
+        <Text style={styles.incidentProperty}>Incident:</Text>
+        <Text style={styles.incidentValue}>{incident.description}</Text>
+
         <Text style={styles.incidentProperty}>Value:</Text>
-        <Text style={styles.incidentValue}>U$ 120.00</Text>
+        <Text style={styles.incidentValue}>
+          {Intl.NumberFormat("en-US", {
+            style: "currency",
+            currency: "USD"
+          }).format(incident.value)}
+        </Text>
       </View>
       <View style={styles.contactBox}>
         <Text style={styles.heroTitle}>Save the day!</Text>
--- src/pages/Incidents/index.js
+++ src/pages/Incidents/index.js
@@ -11,8 +11,8 @@ function Incidents() {
   const [total, setTotal] = useState(0);
   const navigation = useNavigation();
 
-  function navigateToDetail() {
-    navigation.navigate("Detail");
+  function navigateToDetail(incident) {
+    navigation.navigate("Detail", { incident });
   }
   async function loadIncidents() {
     const response = await api.get("incident");
@@ -60,7 +60,7 @@ function Incidents() {
 
             <TouchableOpacity
               style={styles.detailsButton}
-              onPress={navigateToDetail}
+              onPress={() => navigateToDetail(incident)}
             >
               <Text style={styles.detailsButtonText}>Show datails</Text>
               <Feather name="arrow-right" size={16} color="#E02041" />
